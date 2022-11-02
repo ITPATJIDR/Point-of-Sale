4410f9c32ed70ac5bdb595e865a0a249dd9be06f
ITPATJIDR <ittipatjitrada@gmail.com>
2022-11-02 20:01:56 +0700

    connect mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const app = express();
 const PORT = process.env.PORT || 5000
 
@@ -10,6 +12,18 @@ app.use(cors({origin:"http://localhost:3000",credentials:true}));
 app.get("/",(req,res)=>{
 	res.send("Hi")
 })
+
+mongoose.connect(
+	keys.mogodbURI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	(err) =>{
+		if(err) throw err;
+		console.log("Connect to MongoDB");
+	}
+)
 
 app.listen(PORT, ()=>{
 	console.log(`listening on port ${PORT}`)
